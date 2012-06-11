1cfd64f4716724fcbcb6f14daf943ac002508e76
Adria Armejach <miconof@gmail.com>
2012-06-11 14:13:58 +0200

    updated todo plugin

--- plugins/todo.txt/todo.txt.plugin.zsh
+++ plugins/todo.txt/todo.txt.plugin.zsh
@@ -1,11 +1,12 @@
 # Functions
-function t() {
-  if [ $# -eq 0 ]; then
-    todo.sh ls
-  else
-    todo.sh $*
-  fi
-}
+#function t() {
+  #if [ $# -eq 0 ]; then
+    #todo.sh ls
+  #else
+    #todo.sh $*
+  #fi
+#}
 
 # Aliases
+alias t='todo.sh'
 alias n="t ls +next"
